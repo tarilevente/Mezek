0178f8f3a043b5a4f938be4cbc565ca6d0f1098c
Tari Levente <taril88@gmail.com>
2020-06-10 15:41:21 +0200

    logout.php, simply menu_in, menu_out add

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php
+session_start(); 
 
+require_once('config/connect.php'); //database connect
+require_once('config/functions.php'); //using methods
+
+echo file_get_contents("html/header.html");
+
+PrintMenu();
+$con->close();
+echo file_get_contents("html/footer.html");
