285b9bd018044398d945ef71958d22d1d79bbc90
Tari Levente <taril88@gmail.com>
2020-06-11 14:16:36 +0200

    lassan halad a kezdooldal

--- php/indexContent.php
+++ php/indexContent.php
@@ -5,9 +5,9 @@
     </div>
 </div>
 
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-8">
+<div class="container-fluid mainDiv">
+    <div class="row" id="aHonlapCelja">
+        <div class="col-8 ">
             <h2>A honlap célja</h2>
             <h4>Megosztani Veletek az örömöt, böngésszetek kedvetekre! </h4>
             <p>
@@ -22,8 +22,98 @@
         <div class="col-4">
             <img src="public/resources/pics/profile_pics/basic.jpg" alt="IT'S ME">
         </div>
-    </div>
+    </div><!--endof #honlapCelja-->
+    <div class="bg-grey " id="linkek" >
+        <H2>Linkek</H2>
+        <h4 class="pb-3">Amennyiben érdekel Téged a téma, az alábbi linkeken találsz még mezeket. :)</h4>
+        <div class="container">
+            <div class="row text-center">
+                <div class="col-sm-4">
+                    <div class="thumbnail bordered">
+                        <a href="http://stevensfootballshirts.net/index2.html" target="_blank">
+                            <img src="public/resources/pics/links/stevens.jpg" alt="stevensfootballshirts.net"/>
+                        </a>
+                        <p><strong>Steven futball mez gyűjteménye</strong></p>
+                        <p>Szuper kis oldal, nekem nagyon tetszik</p>
+                    </div>
+                </div><!--endof LINK-->
+                <div class="col-sm-4">
+                    <div class="thumbnail bordered">
+                        <a href="https://www.kummerbube.com/" target="_blank">
+                            <img src="public/resources/pics/links/kummerbube_banner.jpg" alt="kummerbube.com"/>
+                        </a>
+                        <p><strong>Egy magángyűjtemény, szuper!</strong></p>
+                        <p>Azért tetszik, mert jól néz ki. </p>
+                    </div>
+                </div><!--endof LINK-->
+                <div class="col-sm-4">
+                    <div class="thumbnail bordered">
+                        <a href="http://fcbtrikots.ch/" target="_blank">
+                            <img src="public/resources/pics/links/trikots.de.jpg" alt="fcbtrikots.ch"/>
+                        </a>
+                        <p><strong>Steven futballmez gyűjteménye</strong></p>
+                        <p>Szuper kis oldal, nekem nagyon tetszik</p>
+                    </div>
+                </div><!--endof LINK-->
+            </div><!--endof ROW-->
+    </div><!--endof #linkek-->
+
+    <div class="bg-info" id="kedvencek">
+        <!-- <h2>A kedvenc mezeim</h2>
+                <div id="myCarousel" class="carousel slide mt-1" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="1"></li>
+                    <li data-target="#myCarousel" data-slide-to="2"></li>
+                    <li data-target="#myCarousel" data-slide-to="3"></li>
+                    <li data-target="#myCarousel" data-slide-to="4"></li>
+                </ol>
+
+                <div class="carousel-inner" role="listbox">
+                    <div class="carousel-item active">
+                        <img src="public/resources/pics/carousel/1.jpg" alt="mez1" width="250px" >
+                    </div>
+                    <div class="carousel-item">
+                        <img src="public/resources/pics/carousel/2.jpg" alt="mez2" width="150px" >
+                    </div>
+                    <div class="carousel-item">
+                        <img src="public/resources/pics/carousel/1.jpg" alt="mez3" width="150px" >
+                    </div>
+                    <div class="carousel-item">
+                        <img src="public/resources/pics/carousel/2.jpg" alt="mez4" width="150px" >
+                    </div>
+                    <div class="carousel-item">
+                        <img src="public/resources/pics/carousel/1.jpg" alt="mez5" width="150px" >
+                    </div>
+                </div>
+
+                <a class="carousel-control-prev" href="#myCarousel" data-slide="prev">
+                    <span class="carousel-control-prev-icon"></span>
+                </a>
+                <a class="carousel-control-next" href="#myCarousel" data-slide="next">
+                    <span class="carousel-control-next-icon">NEXXXXT</span>
+                </a>
+        </div> -->
+    </div><!--endof kedvencek-->
+
+    
+    <div class="" id="kapcsolat">
+        <div class="jumbotron">
+            <h2>Kapcsolat</h2>
+            <h4>Amennyiben szeretnéd felvenni velem a kapcsolatot, üzenj nekem!</h4>
+            <form>
+
+            
+            </form>
+        </div><!--endof -->
+    </div><!--endof kapcsolat-->
 </div>
+
+
+
+
+
+
 
 <?php
 
