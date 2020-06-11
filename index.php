72c0d656fc7200c2ab328bd65023948c6a517e38
Tari Levente <taril88@gmail.com>
2020-06-11 12:39:03 +0200

    navbar felig kesz, haladok lefele

--- index.php
+++ index.php
@@ -7,5 +7,6 @@ require_once('config/functions.php'); //using methods
 echo file_get_contents("html/header.html");
 
 PrintMenu();
+require_once('php/indexContent.php');
 $con->close();
 echo file_get_contents("html/footer.html");
---
+++ php/indexContent.php
@@ -0,0 +1,29 @@
+<div class="container-fluid fullDiv jumbotron" id="bg-pic1" >
+    <div  class="text-center pic1-pos">
+        <h1> Foci mezek</h1>
+        <h3>A gyűjteményem</h3>
+    </div>
+</div>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-8">
+            <h2>A honlap célja</h2>
+            <h4>Megosztani Veletek az örömöt, böngésszetek kedvetekre! </h4>
+            <p>
+                Sziasztok, Kaiser Rezső vagyok. A mezeket nagyon régóta gyűjtöm, a focizás mellett ez az egyik kedvenc elfoglaltságom.<br>
+                .... <br>
+                .... <br>
+                .... 
+            </p>
+            <button>LIKE</button>
+                                <!-- LÁJKOLJ- adatbázisból kiolvassa, lájkszámláló -->
+        </div>
+        <div class="col-4">
+            <img src="public/resources/pics/profile_pics/basic.jpg" alt="IT'S ME">
+        </div>
+    </div>
+</div>
+
+<?php
+
