2d6af2b0cea547bd6d737de55354ddcb3368730f
Tari Levente <taril88@gmail.com>
2020-06-24 17:11:33 +0200

    kategoria felvitele mukodik

--- js/admin_crud.js
+++ js/admin_crud.js
@@ -114,7 +114,7 @@ $(document).ready(function () {
     document.getElementById("cityName").value = "";
   }); //endof reset
 
-  //submit event on new category (post to newTC.php)
+  //submit event on new category (post to newC.php)
   $(document).on("submit", "#newC-form", function (event) {
     event.preventDefault();
 
@@ -127,32 +127,67 @@ $(document).ready(function () {
 
     var errMSG = "";
     var errExists = false;
-    const lig = document.getElementById("newC-Liga-select").value.trim();
+    const liga = document.getElementById("newC-Liga-select").value.trim();
     const Cname = document.getElementById("catName").value.trim();
-
-    if (cat < 0) {
-      errMSG += "Hiba, ilyen kategória nincs! ";
+    if (liga < 0) {
+      errMSG += "Hiba, ilyen liga nincs! ";
       errExists = true;
     }
     if (Cname.length < 2 || Cname.length > 100) {
-      errMSG += "Hiba, a csapatnév nem megfelelő hosszú! ";
+      errMSG += "Hiba, a kategórianév nem megfelelő hosszúságú! ";
       errExists = true;
     }
 
     if (!errExists) {
       $.ajax({
-        url: "php/admin_crud/newTC.php",
+        url: "php/admin_crud/newC.php",
         method: "POST",
         dataType: "text",
-        data: { lig: lig, Cname: Cname },
-        success: function () {
-          console.log("success ágC");
-          succ.innerHTML = "Sikeres bevitel: " + Cname + " !";
+        data: { liga: liga, Cname: Cname },
+        success: function (res) {
+          err.innerHTML = "";
+          err.style.display = "none";
+          succ.innerHTML = "Sikeres bevitel " + Cname + " !";
           succ.style.display = "block";
         },
         error: function (res) {
-          console.log("error ágC");
-          const resJSON = JSON.parse(res);
+          console.log(res.responseText);
+          const resJSON = JSON.parse(res.responseText);
+          if (resJSON.errorCode == 76100) {
+            console.log("no post, error code: 76100");
+            err.innerHTML = resJSON.errorMsg;
+            err.style.display = "block";
+            succ.innerHTML = "";
+            succ.style.display = "none";
+          }
+          if (resJSON.errorCode == 76101) {
+            console.log("no categoryName or no liga, error code: 76101");
+            err.innerHTML = resJSON.errorMsg;
+            err.style.display = "block";
+            succ.innerHTML = "";
+            succ.style.display = "none";
+          }
+          if (resJSON.errorCode == 76102) {
+            console.log("liga not exists, error code: 76102");
+            err.innerHTML = resJSON.errorMsg;
+            err.style.display = "block";
+            succ.innerHTML = "";
+            succ.style.display = "none";
+          }
+          if (resJSON.errorCode == 76103) {
+            console.log("catName too shor or too long, error code: 76103");
+            err.innerHTML = resJSON.errorMsg;
+            err.style.display = "block";
+            succ.innerHTML = "";
+            succ.style.display = "none";
+          }
+          if (resJSON.errorCode == 76104) {
+            console.log("category already exists, error code: 76104");
+            err.innerHTML = resJSON.errorMsg;
+            err.style.display = "block";
+            succ.innerHTML = "";
+            succ.style.display = "none";
+          }
         },
       });
     } else {
@@ -172,101 +207,4 @@ $(document).ready(function () {
     document.getElementById("newC-Liga-select").value = "2";
     document.getElementById("catName").value = "";
   }); //endof reset
-
-  //submit event on new category (post to newC.php)
-  $(document).on("submit", "#newT-form", function (event) {
-    event.preventDefault();
-
-    const err = document.getElementById("errorVanT");
-    const succ = document.getElementById("successVanT");
-    err.innerHTML = "";
-    succ.innerHTML = "";
-    err.style.display = "none";
-    succ.style.display = "none";
-
-    var errMSG = "";
-    var errExists = false;
-    const cat = document.getElementById("newT-Cat-select").value.trim();
-    const Tname = document.getElementById("teamName").value.trim();
-    var cityName = document.getElementById("cityName").value.trim();
-    if (cityName.length < 1) {
-      cityName = "";
-    }
-
-    if (cat < 0) {
-      errMSG += "Hiba, ilyen kategória nincs! ";
-      errExists = true;
-    }
-    if (Tname.length < 2 || Tname.length > 100) {
-      errMSG += "Hiba, a csapatnév nem megfelelő hosszú! ";
-      errExists = true;
-    }
-    if (cityName.length > 100) {
-      errMSG += "Hiba, a város neve túl hosszú!";
-      errExists = true;
-    }
-
-    if (!errExists) {
-      $.ajax({
-        url: "php/admin_crud/newT.php",
-        method: "POST",
-        dataType: "text",
-        data: { cat: cat, Tname: Tname, cityName: cityName },
-        success: function (res) {
-          err.innerHTML = "";
-          err.style.display = "none";
-          succ.innerHTML = "Sikeres bevitel!";
-          succ.style.display = "block";
-        },
-        error: function (res) {
-          const resJSON = JSON.parse(res.responseText);
-          if (resJSON.errorCode == 75200) {
-            console.log("no post, error code: 75200");
-            err.innerHTML = resJSON.errorMsg;
-            err.style.display = "block";
-            succ.innerHTML = "";
-            succ.style.display = "none";
-          }
-          if (resJSON.errorCode == 75201) {
-            console.log("wrong category: 75201");
-            err.innerHTML = resJSON.errorMsg;
-            err.style.display = "block";
-            succ.innerHTML = "";
-            succ.style.display = "none";
-          }
-          if (resJSON.errorCode == 75202) {
-            console.log("wrong teamname, error code: 75202");
-            err.innerHTML = resJSON.errorMsg;
-            err.style.display = "block";
-            succ.innerHTML = "";
-            succ.style.display = "none";
-          }
-          if (resJSON.errorCode == 75203) {
-            console.log("empty category, or empty teamname, error code: 75203");
-            err.innerHTML = resJSON.errorMsg;
-            err.style.display = "block";
-            succ.innerHTML = "";
-            succ.style.display = "none";
-          }
-          if (resJSON.errorCode == 75204) {
-            console.log("cityname is too long, error code: 75204");
-            err.innerHTML = resJSON.errorMsg;
-            err.style.display = "block";
-            succ.innerHTML = "";
-            succ.style.display = "none";
-          }
-          if (resJSON.errorCode == 75205) {
-            console.log("team already exists, error code: 75205");
-            err.innerHTML = resJSON.errorMsg;
-            err.style.display = "block";
-            succ.innerHTML = "";
-            succ.style.display = "none";
-          }
-        },
-      });
-    } else {
-      err.innerHTML = errMSG;
-      err.style.display = "block";
-    }
-  }); //endof new team post
 }); //endof ready()
---
+++ php/admin_crud/newC.php
@@ -0,0 +1,70 @@
+<?php
+require '../../config/connect.php';
+require '../../config/functions.php';
+
+$response              = array();
+$response['error']     = false;
+$response['errorCode'] = "";
+$response['errorMsg']  = "";
+
+if (
+ isset($_POST['liga']) && !empty($_POST['liga']) &&
+ isset($_POST['Cname']) && !empty($_POST['Cname'])
+) {
+//posts are OK
+ $liga  = testInput($_POST['liga']);
+ $Cname = testInput($_POST['Cname']);
+
+ if (
+  !$liga ||
+  !$Cname
+ ) {
+  http_response_code(405);
+  $response['error']     = true;
+  $response['errorMsg']  = "A megadott érték nem lehet csak szóköz! error code: 76101";
+  $response['errorCode'] = 76101;
+ }
+
+ if ($liga < 0 || $liga > 10) {
+  http_response_code(405);
+  $response['error']     = true;
+  $response['errorMsg']  = "A liga nem létezik! error code: 76102";
+  $response['errorCode'] = 75102;
+ }
+
+ if (strlen($Cname) < 2 || strlen($Cname) > 100) {
+  http_response_code(405);
+  $response['error']     = true;
+  $response['errorMsg']  = "Túl rövid, vagy túl hosszú a kategórianév! error code: 76103";
+  $response['errorCode'] = 76103;
+ }
+
+ $usedCategories = array();
+ $sql2           = "SELECT CatName FROM CategoryTable";
+ $res2           = $con->query($sql2);
+ if ($res2) {
+  while ($row = mysqli_fetch_row($res2)) {
+   $usedCategories[] = $row[0];
+  }
+ }
+
+ if (in_array($Cname, $usedCategories)) {
+  //Team already Exists
+  http_response_code(405);
+  $response['error']     = true;
+  $response['errorMsg']  = "A kategória már létezik! error code: 76104";
+  $response['errorCode'] = 76104;
+ }
+ if (false === $response['error']) {
+  $sql = "INSERT INTO `CategoryTable` (`idCategory`, `idLeague`, `catName`) VALUES (NULL, '$liga', '$Cname');";
+  $res = $con->query($sql);
+ }
+} else {
+ //no post exist
+ http_response_code(405);
+ $response['error']     = true;
+ $response['errorMsg']  = "Valami hiba történt! error code: 76100";
+ $response['errorCode'] = 76100;
+}
+
+echo json_encode($response, JSON_UNESCAPED_UNICODE);
